3d1544b777c17e0ccfe378cac4d9de33d12a9aae
MarcusTL12 <marcuspaafjellet@gmail.com>
2021-09-27 12:03:20 +0200

    Day5

--- src/day5.c
+++ src/day5.c
@@ -7,7 +7,8 @@ const char *input = "ffykfhsq";
 // const char *input = "abc";
 
 void d5p1() {
-    char md5_buf[32];
+    char md5_buf[16];
+    char hex_buf[6];
     char text_buf[32];
     memset(text_buf, 0, 32);
 
@@ -21,18 +22,21 @@ void d5p1() {
     for (int i = 1, j = 0; j < 8; i++) {
         int num_len = sprintf(num_buf, "%i", i);
         md5(md5_buf, text_buf, inp_len + num_len);
-        if (!memcmp("00000", md5_buf, 5)) {
-            pass_buf[j] = md5_buf[5];
-            printf("%c\n", md5_buf[5]);
+        raw2hex(hex_buf, md5_buf, 3);
+        if (!memcmp("00000", hex_buf, 5)) {
+            pass_buf[j] = hex_buf[5];
+            printf("%c", hex_buf[5]);
+            fflush(stdout);
             j++;
         }
     }
 
-    printf("%.*s\n", 8, pass_buf);
+    printf("\n");
 }
 
 void d5p2() {
-    char md5_buf[32];
+    char md5_buf[16];
+    char hex_buf[8];
     char text_buf[32];
     memset(text_buf, 0, 32);
 
@@ -42,20 +46,24 @@ void d5p2() {
     char *num_buf = text_buf + inp_len;
 
     char pass_buf[8];
-    memset(pass_buf, ' ', 8);
+    memset(pass_buf, '_', 8);
+    printf("%.*s", 8, pass_buf);
+    fflush(stdout);
 
     for (int i = 1, j = 0; j < 8; i++) {
         int num_len = sprintf(num_buf, "%i", i);
         md5(md5_buf, text_buf, inp_len + num_len);
-        if (!memcmp("00000", md5_buf, 5)) {
-            int ind = md5_buf[5] - '0';
-            if (pass_buf[ind] == ' ') {
-                pass_buf[ind] = md5_buf[6];
+        raw2hex(hex_buf, md5_buf, 4);
+        if (!memcmp("00000", hex_buf, 5)) {
+            int ind = hex_buf[5] - '0';
+            if (0 <= ind && ind < 8 && pass_buf[ind] == '_') {
+                pass_buf[ind] = hex_buf[6];
                 j++;
-                printf("%.*s\n", 8, pass_buf);
+                printf("\r%.*s", 8, pass_buf);
+                fflush(stdout);
             }
         }
     }
 
-    // printf("%.*s\n", 8, pass_buf);
+    printf("\n");
 }
